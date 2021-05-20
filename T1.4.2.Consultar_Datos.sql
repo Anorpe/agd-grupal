33b860961103a8b4b035379464911469672776c5
Diego Eusse <diego.eusse@globant.com>
2021-05-19 21:43:46 -0500

    Creacion archivo consultar datos Mongo

---
+++ T1.4.2.Consultar_Datos.sql
@@ -0,0 +1,16 @@
+/* Consultas de olist_geolocation */
+
+/* Consultas de olist_customers */
+
+/* Consultas de olist_sellers */
+
+/* Consultas de olist_orders */
+
+/* Consultas de olist_order_reviews */
+
+/* Consultas de olist_order_payments */
+
+/* Consultas de olist_products */
+
+/* Consultas de olist_order_items */
+
